a9d68c42a987828afab1645ac097aab34ef460e3
bobhatepradip <bobhate.pradip@gmail.com>
2019-12-18 17:18:24 +0530

    1 file modified, 3 added, 3 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
-import { ServiceCartService, ServiceService } from './service.service';
+import { CartComponent } from './cart/cart.component';
 import { CartService } from './cart.service';
 
 @NgModule({
@@ -26,10 +26,11 @@ import { CartService } from './cart.service';
     TopBarComponent,
     ProductListComponent,
     ProductAlertsComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    CartComponent
   ],
   bootstrap: [ AppComponent ],
-  providers: [ServiceCartService, CartService, ServiceService]
+  providers: [CartService]
 })
 export class AppModule { }
 
